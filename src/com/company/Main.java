9cd33c3d1a83f812c6f1200eb4be6af615e7ac50
noorkhan3226 <noorafghan3226@gmail.com>
2021-12-14 21:21:53 -0500

    fiurst commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        String a="habibi";
+        System.out.println(a);
+        Thread.sleep(2000);
+    }
+}
